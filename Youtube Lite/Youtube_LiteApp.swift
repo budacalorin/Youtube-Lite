7c0f3ae22d6b6c3a5bd0c8c71e306083d54c990f
lorinbudaca@gmail.com <H@Minecraft200>
2021-07-15 22:50:20 +0300

    Initial Commit

---
+++ Youtube Lite/Youtube_LiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Youtube_LiteApp.swift
+//  Youtube Lite
+//
+//  Created by Lorin Budaca on 15.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct Youtube_LiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
